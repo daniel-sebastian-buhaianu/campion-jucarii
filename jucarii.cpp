f217304f3c2914ee304a7fa9e7160197072e3912
Daniel-Sebastian Buhaianu <128343993+daniel-sebastian-buhaianu@users.noreply.github.com>
2023-12-07 10:38:58 +0200

    Update jucarii.cpp - 80

--- jucarii.cpp
+++ jucarii.cpp
@@ -5,26 +5,21 @@ void citesteDateleDeIntrare();
 void genereazaPermutari();
 int main()
 {
-	citesteDateleDeIntrare();
-	genereazaPermutari();
-	return 0;
+    citesteDateleDeIntrare();
+    genereazaPermutari();
+    return 0;
 }
 bool esteSolutie(int* a)
 {
-	// verifica daca exista un subsir crescator
-	// de lungime K in vectorul a
-	int lgmax[N], nrmin[N];
+    // verifica daca exista un subsir crescator
+    // de lungime K in vectorul a
+    int lgmax[N], nrmin[N];
     lgmax[0] = 1, nrmin[0] = a[0];
     for (int i = 1; i < N; i++)
     {
         if (a[i] > nrmin[i-1])
         {
             lgmax[i] = lgmax[i-1]+1;
-            nrmin[i] = a[i];
-        }
-        else if (lgmax[i-1] == 1)
-        {
-            lgmax[i] = 1;
             nrmin[i] = a[i];
         }
         else
@@ -47,37 +42,37 @@ bool esteSolutie(int* a)
 }
 void afiseazaSolutie(int* a)
 {
-	ofstream g("jucarii.out");
-	for (int i = 0; i < N; i++)
-		g << a[i] << ' ';
-	g.close();
+    ofstream g("jucarii.out");
+    for (int i = 0; i < N; i++)
+        g << a[i] << ' ';
+    g.close();
 }
 void genereazaPermutari()
 {
-	int a[N], i, j;
-	for (i = 0; i < N; i++) a[i] = i+1;
-	while (1)
-	{
-		if (esteSolutie(a))
-		{
-			NS++;
-			if (NS == P)
-			{
-				afiseazaSolutie(a);
-				return;
-			}
-		}
-		for (i = N-1; i > 0 && a[i] < a[i-1]; i--);
-		if (i == 0) break;
-		for (j = i-1, i = N-1; a[i] < a[j]; i--);
-		swap(a[i], a[j]);
-		for (i = j+1, j = N-1; i < j; i++, j--)
-			swap(a[i], a[j]);
-	}
+    int a[N], i, j;
+    for (i = 0; i < N; i++) a[i] = i+1;
+    while (1)
+    {
+        if (esteSolutie(a))
+        {
+            NS++;
+            if (NS == P)
+            {
+                afiseazaSolutie(a);
+                return;
+            }
+        }
+        for (i = N-1; i > 0 && a[i] < a[i-1]; i--);
+        if (i == 0) break;
+        for (j = i-1, i = N-1; a[i] < a[j]; i--);
+        swap(a[i], a[j]);
+        for (i = j+1, j = N-1; i < j; i++, j--)
+            swap(a[i], a[j]);
+    }
 }
 void citesteDateleDeIntrare()
 {
-	ifstream f("jucarii.in");
-	f >> N >> K >> P;
-	f.close();
+    ifstream f("jucarii.in");
+    f >> N >> K >> P;
+    f.close();
 }
